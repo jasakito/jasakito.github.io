17ab90793e96c38c32059e426ae4f2d7ca644c73
jasakito <2303686406@qq.com>
2025-03-22 19:40:05 +0800

    Delete homework_No.1 directory

--- homework_No.1/scriptA.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('playButton').addEventListener('click', function() {
-  var audio = document.getElementById('myMusic');
-    if (audio.paused) {
-       audio.play();
-       this.textContent = '暂停';
-      } else {
-        audio.pause();
-         this.textContent = '播放'; }
-       });
